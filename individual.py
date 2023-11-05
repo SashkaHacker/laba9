a64b943b72cc279becc5bffe7d3ab74efd067fa5
SashkaHacker <am942081@gmail.com>
2023-11-05 22:14:45 +0300

    update

--- individual.py
+++ individual.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from datetime import datetime
+import sys
 
 # вариант - 11
 
@@ -15,41 +16,46 @@ if __name__ == "__main__":
         command = input(">>> ").lower()
 
         # Выполнить действие в соответствие с командой.
-        if command == 'exit':
-            break
+        match command:
+            case 'exit':
+                break
 
-        elif command == 'add':
-            # Запросить данные о работнике.
-            surname = input('Фамилия: ')
-            name = input("Имя: ")
-            phone = input("Номер телефона: ")
-            date = input('Дата рождения (число:месяц:год): ').split(':')
+            case 'add':
+                # Запросить данные о работнике.
+                surname = input('Фамилия: ')
+                name = input("Имя: ")
+                phone = input("Номер телефона: ")
+                date = input('Дата рождения (число:месяц:год): ').split(':')
 
-            # Создать словарь.
-            dct = {'surname': surname,
-                   'name': name,
-                   'phone': phone,
-                   'date': date}
-            lst.append(dct)
+                # Создать словарь.
+                dct = {'surname': surname,
+                       'name': name,
+                       'phone': phone,
+                       'date': date}
+                lst.append(dct)
 
-            # Сортировка списка словарей
-            lst.sort(key=lambda x:
-            datetime.strptime('-'.join(x['date']), '%d-%m-%Y'))
-            print(lst)
+                # Сортировка списка словарей
+                lst.sort(key=lambda x:
+                datetime.strptime('-'.join(x['date']), '%d-%m-%Y'))
 
-        elif command == 'phone':
-            numbers_phone = input()
-            fl = True
-            for i in lst:
-                if i['phone'] == numbers_phone:
-                    print(f"Фамилия: {i['surname']}\n"
-                          f"Имя: {i['name']}\n"
-                          f"Номер телефона: {i['phone']}\n"
-                          f"Дата рождения: {':'.join(i['date'])}")
-                    fl = False
-                    break
-            if fl:
-                print("Человека с таким номером телефона нет в списке.")
+            case 'phone':
+                numbers_phone = input('Введите номер телефона')
+                fl = True
+                for i in lst:
+                    if i['phone'] == numbers_phone:
+                        print(f"Фамилия: {i['surname']}\n"
+                              f"Имя: {i['name']}\n"
+                              f"Номер телефона: {i['phone']}\n"
+                              f"Дата рождения: {':'.join(i['date'])}")
+                        fl = False
+                        break
+                if fl:
+                    print("Человека с таким номером телефона нет в списке.")
 
-        else:
-            print(f"Неизвестная команда {command}", file=sys.stderr)
+            case 'help':
+                print("add - добавление нового работника\n"
+                      "phone - данные о работнике по его номеру телефона\n"
+                      "exit - завершение программы")
+
+            case _:
+                print(f"Неизвестная команда {command}", file=sys.stderr)
